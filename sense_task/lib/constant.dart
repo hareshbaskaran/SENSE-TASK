460fd2b3cebaac93da4200c08dec33d6f0bddb58
hareshbaskaran <baskaranharesh@gmail.com>
2022-07-21 22:05:13 +0530

    ishu login unfixed

--- sense_task/lib/constant.dart
+++ sense_task/lib/constant.dart
@@ -2,4 +2,5 @@ const MONGO_URL =
     "mongodb+srv://tester:RMfosk0Ovr9JkTsJ@cluster0.8vdgc.mongodb.net/?retryWrites=true&w=majority";
 const ADMIN_COLLECTION = "admin";
 const TASK_COLLECTION = "task";
+const USER_COLLECTION = "user";
 //RMfosk0Ovr9JkTsJ
